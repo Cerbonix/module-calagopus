6bbe33a351ace4f12192d4a706b8e64715b66578
Allionix <a.eteocle@cerbonix.net>
2026-08-29 12:18:17 +0200

    chore: add Cerbonix copyright header to php files

--- modules/calagopus/src/CalagopusServerType.php
+++ modules/calagopus/src/CalagopusServerType.php
@@ -1,5 +1,11 @@
 <?php
 
+/*
+ * This file is part of the Calagopus provisioning module for CLIENTXCMS.
+ *
+ * Copyright (c) 2026 Cerbonix - https://cerbonix.net
+ */
+
 namespace App\Modules\Calagopus;
 
 use App\Abstracts\AbstractServerType;
--- modules/calagopus/src/CalagopusServiceProvider.php
+++ modules/calagopus/src/CalagopusServiceProvider.php
@@ -1,5 +1,11 @@
 <?php
 
+/*
+ * This file is part of the Calagopus provisioning module for CLIENTXCMS.
+ *
+ * Copyright (c) 2026 Cerbonix - https://cerbonix.net
+ */
+
 namespace App\Modules\Calagopus;
 
 class CalagopusServiceProvider extends \App\Extensions\BaseModuleServiceProvider
